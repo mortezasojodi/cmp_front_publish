ae3f83a2fed15b7617070725b5f20f01e4624ca1
morteza <mortezasojodi70@gmail.com>
2024-09-20 16:19:39 +0330

    .

--- components/forms/title/title.tsx
+++ components/forms/title/title.tsx
@@ -1,15 +1,16 @@
+"use client"
 import styles from "@/components/forms/title/title.module.css";
 import Image from "next/image";
-
-
 
 export default function Title({ title, icon, onPress = null }) {
   return (
     <div className={styles.title}>
-      {icon && <Image src={icon} width={"36"} height={"36"} alt="broom" onClick={() => (onPress) ? onPress() : null} />}
+      {icon && (
+        <div onClick={() => (onPress ? onPress() : null)} style={{ cursor: "pointer" }}>
+          <Image src={icon} width={36} height={36} alt="broom" />
+        </div>
+      )}
       <h1>{title}</h1>
     </div>
   );
 }
-
-
